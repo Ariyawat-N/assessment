1a80a35017dfe7d219ccdb29eed766c80fa6f8f8
ging <ariyawat.nky@gmail.com>
2024-02-20 22:35:10 +0700

    add usertickets request and response Dto

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/userticket/dto/UserTicketsRequestDto.java
@@ -0,0 +1,18 @@
+package com.kbtg.bootcamp.posttest.userticket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserTicketsRequestDto {
+    private List<String> tickets;
+    private Integer count;
+    private Integer cost;
+}
