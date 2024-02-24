b6498309097d879a970c611db9dbb0ff25200bb1
ging <ariyawat.nky@gmail.com>
2024-02-25 00:19:00 +0700

    add validate to userTicketController

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/userticket/controller/UserTicketController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/userticket/controller/UserTicketController.java
@@ -24,12 +24,12 @@ public class UserTicketController {
     @PostMapping("/users/{userId}/lotteries/{ticketId}")
     public ResponseEntity<UserTicketsRequestDto> buyLotteries(
             @PathVariable("userId")
-//            @Pattern(regexp = "^0\\d{9}$", message = "must be a number only  and start with '0'")
-//            @Size(min = 10, max = 10, message = "must be a 10 digit")
+            @Pattern(regexp = "^0\\d{9}$", message = "must be a number only  and start with '0'")
+            @Size(min = 10, max = 10, message = "must be a 10 digit")
             String userId,
             @PathVariable("ticketId")
-//            @Pattern(regexp = "^\\d{6}$", message = "must be a number only")
-//            @Size(min = 6, max = 6, message = "must be a 6 digit")
+            @Pattern(regexp = "^\\d{6}$", message = "must be a number only")
+            @Size(min = 6, max = 6, message = "must be a 6 digit")
             String ticketId) {
         return new ResponseEntity<>(userTicketService.buyLotteries(userId, ticketId), HttpStatus.OK);
     }
@@ -38,8 +38,8 @@ public class UserTicketController {
     @GetMapping("/users/{userId}/lotteries")
     public ResponseEntity<UserTicketResponseDto> getLotteriesByUserId(
             @PathVariable("userId")
-//            @Pattern(regexp = "^0\\d{9}$", message = "must be a number only  and start with '0'")
-//            @Size(min = 10, max = 10, message = "must be a 10 digit")
+            @Pattern(regexp = "^0\\d{9}$", message = "must be a number only  and start with '0'")
+            @Size(min = 10, max = 10, message = "must be a 10 digit")
             String userId) {
         return new ResponseEntity<>(userTicketService.getLotteriesByUserId(userId), HttpStatus.OK);
     }
@@ -49,12 +49,12 @@ public class UserTicketController {
     @DeleteMapping("/users/{userId}/lotteries/{ticketId}")
     public ResponseEntity<LotteryResponseDto> deleteLotteriesByUserId(
             @PathVariable("userId")
-//            @Pattern(regexp = "^0\\d{9}$", message = "must be a number only  and start with '0'")
-//            @Size(min = 10, max = 10, message = "must be a 10 digit")
+            @Pattern(regexp = "^0\\d{9}$", message = "must be a number only  and start with '0'")
+            @Size(min = 10, max = 10, message = "must be a 10 digit")
             String userId,
             @PathVariable("ticketId")
-//            @Pattern(regexp = "^\\d{6}$", message = "must be a number only")
-//            @Size(min = 6, max = 6, message = "must be a 6 digit")
+            @Pattern(regexp = "^\\d{6}$", message = "must be a number only")
+            @Size(min = 6, max = 6, message = "must be a 6 digit")
             String ticketId) {
         return new ResponseEntity<>(userTicketService.deleteLotteriesByUserId(userId, ticketId), HttpStatus.OK);
     }
