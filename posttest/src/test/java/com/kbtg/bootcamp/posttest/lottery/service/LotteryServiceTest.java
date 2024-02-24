7d3315437af1dbec8b9f13d6feca90c29e4d44af
ging <ariyawat.nky@gmail.com>
2024-02-24 21:59:37 +0700

    Implement testListALlLotteries in LotteryService

--- posttest/src/test/java/com/kbtg/bootcamp/posttest/lottery/service/LotteryServiceTest.java
+++ posttest/src/test/java/com/kbtg/bootcamp/posttest/lottery/service/LotteryServiceTest.java
@@ -1,18 +1,21 @@
 package com.kbtg.bootcamp.posttest.lottery.service;
 
+import com.kbtg.bootcamp.posttest.lottery.dto.LotteryListResponseDto;
 import com.kbtg.bootcamp.posttest.lottery.dto.LotteryRequestDto;
 import com.kbtg.bootcamp.posttest.lottery.dto.LotteryResponseDto;
 import com.kbtg.bootcamp.posttest.lottery.entity.Lottery;
 import com.kbtg.bootcamp.posttest.lottery.exception.LotteryExistException;
 import com.kbtg.bootcamp.posttest.lottery.repository.LotteryRepository;
-import com.kbtg.bootcamp.posttest.lottery.service.LotteryService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -54,5 +57,20 @@ public class LotteryServiceTest {
         assertThrows(LotteryExistException.class, () -> lotteryService.createLottery(requestDto));
         verify(lotteryRepository, times(1)).findByTicket("012345");
         verify(lotteryRepository, never()).save(any(Lottery.class));
+    }
+
+    @Test
+    public void testListAllLotteries() {
+        // Mock data
+        List<Lottery> mockLotteries = Arrays.asList(new Lottery("012345", 80, 1), new Lottery("054321", 80, 1));
+        when(lotteryRepository.findAll()).thenReturn(mockLotteries);
+
+        // Call the service method
+        LotteryListResponseDto responseDto = lotteryService.listAllLotteries();
+
+
+        // Verify the result
+        assertEquals(2, responseDto.getTickets().size());
+        assertEquals(mockLotteries.stream().map(Lottery::getTicket).collect(Collectors.toList()), responseDto.getTickets());
     }
 }
